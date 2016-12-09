1dded73c670412a7d6749b90b6d213bed327e950
Julie Frazelle <j.frazelle@form.willow.if3>
2016-12-09 12:00:13 +0100

    save project

---
+++ Assets/FurBall2D/Scripts/PlateformDestroyer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateformDestroyer : MonoBehaviour {
+
+    public GameObject plateformDestructionPoint;
+
+	// Use this for initialization
+	void Start () {
+
+        plateformDestructionPoint = GameObject.Find("PlateformDestructionPoint");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+       if (transform.position.x < plateformDestructionPoint.transform.position.x)
+       {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+       }
+	}
+}
---
+++ Assets/FurBall2D/Scripts/PlateformGenerater.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateformGenerater : MonoBehaviour {
+
+    public GameObject thePlateform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float plateformWidth;
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+    public ObjectPooler theObjectPool;
+
+	// Use this for initialization
+	void Start () {
+        plateformWidth = thePlateform.GetComponent<BoxCollider2D>().size.x;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < generationPoint.position.x)
+        {
+            distanceBetween = Random.Range (distanceBetweenMin,distanceBetweenMax);
+
+            transform.position = new Vector3(transform.position.x + plateformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+            //Instantiate(thePlateform, transform.position, transform.rotation);
+            GameObject newPlateform = theObjectPool.GetPooledObject();
+
+            newPlateform.transform.position = transform.position;
+            newPlateform.transform.rotation = transform.rotation;
+            newPlateform.SetActive (true);
+
+
+        }
+	
+	}
+}
